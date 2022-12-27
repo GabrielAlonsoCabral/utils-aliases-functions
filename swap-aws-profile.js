e61e2b95c520939c68faf6712352b1e8358bda0d
Gabriel Cabral <gabriel.alonso.dev@icloud.com>
2022-12-27 19:24:49 -0300

    feat: swap-aws-profile.js

---
+++ swap-aws-profile.js
@@ -0,0 +1,33 @@
+const {relative, join} = require("path")
+const fs = require("fs").promises;
+const execSync = require('child_process').execSync;
+
+const ALIASES_PATH = execSync('printenv ALIASES_PATH',{encoding:'utf8'})
+const AWS_PATH = execSync('printenv AWS_PATH',{encoding:'utf8'})
+const AWS_RELATIVE_PATH = relative(ALIASES_PATH, AWS_PATH).replace("\n","")
+
+const JSON_CREDENTIALS_PATH =join('.','.credentials','aws.credentials.json')
+
+async function swapAWSProfile(){
+    const credentialsText = await fs.readFile(JSON_CREDENTIALS_PATH, {encoding:'utf-8'});
+    const credentialsJson = JSON.parse(credentialsText)
+    const newCredentials ={};
+
+    let resultString=""
+        
+    Object.keys(credentialsJson).forEach((profile,index)=>{
+        const credential = credentialsJson[profile]
+
+            const profileAlias = credential.is_default ? `[${profile}]` : '[default]';
+            resultString+=`${index===0?'':'\n'}${profileAlias}\naws_access_key_id=${credential.aws_access_key_id}\naws_secret_access_key=${credential.aws_access_key_id}`
+            credential.is_default = !credential.is_default;   
+            
+            newCredentials[profile]=credential
+    })
+
+
+    await fs.writeFile(join(AWS_RELATIVE_PATH,'credentials'),resultString)        
+    await fs.writeFile(JSON_CREDENTIALS_PATH, JSON.stringify(newCredentials));
+}
+
+swapAWSProfile()
